a301c7629443b67ceb41898fee97342087f6a5f4
Phil Belinsky <philbelinsky@yahoo.com>
2025-07-14 11:15:23 -0400

    cron.js added

--- src/server.js
+++ src/server.js
@@ -2,15 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import transactionsRoute from "./routes/transactions.route.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
 
+if(process.env.NODE_ENV === "production")job.start();
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "OK" });
+})
 
 
 app.use("/api/transactions", transactionsRoute)
